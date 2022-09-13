381bee302a4b1bb50872773c2f6463c072861963
carreuky <carreuky@gmail.com>
2022-09-13 20:48:19 +0300

    done with intro

--- src/Components/intro.jsx
+++ src/Components/intro.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
+import { FaFacebook,FaTwitter,FaInstagram,FaLinkedinIn} from "react-icons/fa";
+
 
 export default function Intro() {
+
+    let iconStyles = { fontSize: "1.1em" };
   return (
     <div>
         <div className='p-2 text-white flex flex-wrap justify-between bg-slate-800'>
             <div>
-            <p>EMPOWERING PEOPLE</p>
+            <p className='pl-4'>EMPOWERING PEOPLE</p>
             </div>
             <div className='flex px-2 flex-wrap'>
-                <p className=' px-2'>0710363256</p>
-                <p className=' px-2'>Derrick Email</p>
-                <ul className='flex  flex-wrap'>
-                    <li className=' px-2'><a href=''>Facebook</a></li>
-                    <li className=' px-2'><a href=''></a>Twitter</li>
-                    <li className=' px-2'><a href=''></a>iNSTA</li>
-                    <li className=' px-2'><a href=''></a>LinkIN</li>
+                <p className=' px-2'>+254704775938</p>
+                <p className=' px-2 pt-1 text-xs'>WACHIRAMUTATHI@GMAIL.COM</p>
+                <ul className='flex  flex-wrap p-1 px-2'>
+                    <li className='px-2 '><a href='http://web.facebook.com/IamDMWachira' target="_blank"><FaFacebook style={iconStyles}/></a></li>
+                    <li className='px-2'><a className='icon' href=''><FaTwitter style={iconStyles}/></a></li>
+                    <li className='px-2'><a className='icon' href=''><FaInstagram style={iconStyles}/></a></li>
+                    <li className='px-2'><a className='icon' href=''><FaLinkedinIn style={iconStyles}/></a></li>
 
                 </ul>
                 
