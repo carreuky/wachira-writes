607fad2db74f1780760a8427e16cd0f50274c834
carreuky <carreuky@gmail.com>
2022-09-13 19:58:02 +0300

    component navbar and intro

---
+++ src/Components/intro.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function Intro() {
+  return (
+    <div>
+        <div className='p-2 text-white flex flex-wrap justify-between bg-slate-800'>
+            <div>
+            <p>EMPOWERING PEOPLE</p>
+            </div>
+            <div className='flex px-2 flex-wrap'>
+                <p className=' px-2'>0710363256</p>
+                <p className=' px-2'>Derrick Email</p>
+                <ul className='flex  flex-wrap'>
+                    <li className=' px-2'><a href=''>Facebook</a></li>
+                    <li className=' px-2'><a href=''></a>Twitter</li>
+                    <li className=' px-2'><a href=''></a>iNSTA</li>
+                    <li className=' px-2'><a href=''></a>LinkIN</li>
+
+                </ul>
+                
+
+            </div>
+        </div>
+    </div>
+  )
+}
