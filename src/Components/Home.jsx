8101deab781dc2a45e1ad544303ab9719a9a679f
carreuky <carreuky@gmail.com>
2022-09-20 11:59:07 +0300

    fantastic slider

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,54 +1,45 @@
-import React from "react";
-import derro from "../images/mwenyewe-r.png";
-import { NavLink } from "react-router-dom";
-import { FaCheckCircle } from "react-icons/fa";
+import React,{useEffect,useState} from "react";
+import m1 from '../images/m1.jpg'
+import m2 from '../images/m2.jpg'
+import m3 from '../images/m3.jpg'
+import m4 from '../images/m4.jpg'
+import m5 from '../images/m5.jpg'
+import m6 from '../images/m6.jpg'
+
+
+const images =[m1,m2,m3,m4,m5,m6]
 
 export default function Home() {
-  const myStyle = {
-    marginTop: "-130px",
-  };
-   const icon= <FaCheckCircle
-   style={{
-     fontSize: "30px",
-     color: "yellow",
-     marginRight: "10px",
-     marginTop:'4px'
-   }}
- />
 
+const [index, setIndex]=useState(0)
+
+useEffect(()=>{
+  const setImage=setInterval(()=>{
+    setIndex(index=>index + 1)
+  },4000)
+  return()=>{
+    clearInterval(setImage)
+  }
+},[])
+
+function setToZero(){
+ if  (index === 5){
+  setIndex(0)
+ }
+}
+
+setToZero()
   return (
     <div>
-      <div className=" bg-gray-900 flex flex-wrap text-center sm:text-left items-center">
-        <div className="pl-24">
-          <h1 className=" text-6xl font-headline tracking-tight font-extrabold text-white leading-snug sm:leading-normal">
-            MUTATHI WACHIRA.
-          </h1>
-          <p className=" text-3xl text-amber-400 flex">Public Speaker </p>
-          <p className="text-3xl text-amber-400 flex">Author</p>
-          <p className="text-3xl text-amber-400 flex">Speech writer</p>
-          <p className="lg:w-1/2 text-gray-600 lg:text-lg sm:text-base">
-            {/* Give something to right i add hapa nimekosa kitu ama nime idea. */}
-          </p>
-          <div className="mt-12">
-            {" "}
-            <NavLink to="/about">
-              <a
-                href="#_"
-                class="px-10 py-4 mt-4 text-xl font-semibold text-center text-white transition duration-300 rounded-lg hover:from-amber-600 hover:to-pink-600 ease bg-gradient-to-br from-yellow-500 to-pink-500 md:w-auto"
-              >
-                Learn More
-              </a>
-            </NavLink>
-          </div>
-        </div>
-        <div className="mr-40 sm:mr-0  sm:ml-16 sm:px-48 lg:px-0 w-auto">
-          <img
-            className="sm:w-full object-cover  lg:w-96 rounded-md transform transition"
-            src={derro}
-            alt="Image of plants"
-          />
-        </div>
-      </div>
+  <div
+      className="bg-hero-pattern  bg-no-repeat bg-cover mx-auto bg-gray-900 pl-10"
+      style={{
+        height: '80vh',
+        backgroundImage: `linear-gradient(to right bottom, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.9)), url(${images[index]})`,
+      }}
+    >
+
+    </div>
     </div>
   );
 }
--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
 
 export default function Services() {
   return (
-    <div>Services</div>
-  )
+    <div>
+      <div class="text-center ">
+        <div class="container">
+          <h1 class="font-black text-25px md:text-35px lg:text-40px xl:text-48px 2xl:text-54px text-black-color mb-8">
+            The <span class="text-primary-color">Services</span> We Provide
+          </h1>
+          <p class="lg:text-16px md:text-15px text-13px lg:max-w-2xl lg:mx-auto leading-7 md:leading-8 text-optional-color">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            imperdiet nulla duis ac. Id massa scelerisque venenatis, massa
+            gravida donec orci.
+          </p>
+        </div>
+       
+      </div>
+    </div>
+  );
 }
